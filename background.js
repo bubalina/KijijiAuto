b47c8dbe7817f612949d9c2bb3a1d6568adb13a0
bubalina <70701167+bubalina@users.noreply.github.com>
2023-05-11 01:30:23 -0600

    Update background.js
    Added description of code fuction

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+/*
+ * Kijiji Auto Wholesale Price Comparison Extension
+ *
+ * This Chrome extension listens for the user to navigate to Kijiji Auto and extracts
+ * the search query parameters. It then sends an HTTP request to ICBC Canadian
+ * Black Book to retrieve the wholesale prices for each vehicle in the search
+ * results. The extension then displays the results in a table, including
+ * the Year, Make, Model, Trim, Style, KM, Region, Kijiji Price, and
+ * Wholesale Price for each vehicle.
+ *
+ * The extension is activated by clicking on the Chrome extension icon while on a Kijiji Auto page.
+ *
+ * Limitations:
+ * - The extension only works for Kijiji Auto searches in Canada
+ * - The extension only displays wholesale prices for vehicles in British Columbia
+ * - The extension only displays prices for vehicles in the following categories:
+ *   Cars & Trucks, SUVs & Crossovers, Vans, and Classic Cars
+ * 
+ * Copyright (c) Martha Tessema, 2023
+ * Author: Martha Tessema (https://github.com/bubalina)
+ * Version: 1.0
+ * Date: May 10, 2023
+ */
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && /^https:\/\/www.kijijiautos.ca\//.test(tab.url)) {
     const searchParams = new URLSearchParams(tab.url.split('?')[1]);
